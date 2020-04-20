6a87596215ce1bcfa407d2cfb703ccc62d08d019
Malwina8 <48411653+Malwina8@users.noreply.github.com>
2020-04-20 21:48:18 +0200

    Change files extension from html to hbs

--- server.js
+++ server.js
@@ -1,40 +1,36 @@
 const express = require('express');
 const path = require('path');
-
-const app = express();
-
 const hbs = require('express-handlebars');
 
-app.engine('.hbs', hbs());
-app.set('view engine', '.hbs');
-
+const app = express();
+app.engine('hbs', hbs());
+app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname + '/public')));
+
+app.get('/', (req, res) => {
+  res.render('index', { layout: false });
+});
 
 app.get('/hello/:name', (req, res) => {
   res.render('hello', { layout: false, name: req.params.name });
 });
 
-app.get('/', (req, res) => {
-  res.render('index');
-});
-
 app.get('/about', (req, res) => {
-  res.render('about');
+  res.render('about', { layout: false });
 });
 
 app.get('/contact', (req, res) => {
-  res.render('contact');
+  res.render('contact', { layout: false });
 });
 
 app.get('/info', (req, res) => {
-  res.render('info');
+  res.render('info', { layout: false });
 });
 
 app.get('/history', (req, res) => {
-  res.render('history');
+  res.render('history', { layout: false });
 });
-
 
 app.use((req, res) => {
   res.status(404).send('404 not found...');
